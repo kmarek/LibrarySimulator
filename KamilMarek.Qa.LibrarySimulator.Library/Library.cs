d8946d1272bfd22f866271e22cebc60f2fdd192f
KamilMarek-IV <kamil.marek@gmail.com>
2022-05-31 12:20:44 +0200

    added solution with placeholders for classes and methods required

---
+++ KamilMarek.Qa.LibrarySimulator.Library/Library.cs
@@ -0,0 +1,48 @@
+﻿using KamilMarek.Qa.LibrarySimulator.Library.Items;
+using KamilMarek.Qa.LibrarySimulator.Library.Users;
+
+namespace KamilMarek.Qa.LibrarySimulator.Library
+{
+    public class Library
+    {
+        public void AddUserToLibrary(params User[] users)
+        {
+
+        }
+
+        public void AddItemToLibrary(params Item[] items)
+        {
+
+        }
+
+        public bool RentItemToUser(Item item, User user)
+        {
+            return false;
+        }
+
+        public void PrintListOfMagazines()
+        {
+
+        }
+
+        public void PrintListOfBooks()
+        {
+
+        }
+
+        public void PrintListOfUsers()
+        {
+
+        }
+
+        public void ImportItemsFromFile(string csvFile)
+        {
+
+        }
+
+        public void ExportUsersWithItemsToFile(string csvFile)
+        {
+
+        }
+    }
+}
---
+++ KamilMarek.Qa.LibrarySimulator.Library/Users/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamilMarek.Qa.LibrarySimulator.Library.Users
+{
+    public class Student : User
+    {
+    }
+}
